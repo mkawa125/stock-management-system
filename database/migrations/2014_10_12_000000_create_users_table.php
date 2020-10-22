a1ca9fe1663fd2eebadf379402f9004a7a0edda5
Dahabu Saidi Mkawa <dahabusaidi@gmail.com>
2020-10-22 08:10:35 +0300

    Add users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Emadadly\LaravelUuid\Uuids;
 
 class CreateUsersTable extends Migration
 {
@@ -14,13 +15,22 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            /** Use softDeletes */
+            $table->softDeletes();
+
+            /** Asign primary ke */
+            $table->primary('id');
+
+
         });
     }
 
